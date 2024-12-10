1ece4c2c48f1c0829130cb51d4952525dddd085b
AidanH567 <herstikaidan@gmail.com>
2024-12-10 19:04:41 +1100

    returning files to porject

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+let dbConnect = require("./dbConnect");
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// Import the routes
+let userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
+
+let postsRoutes = require("./routes/postsRoutes.js");
+app.use("/api/posts", postsRoutes);
+
+// Add routes for likes and comments
+let likesRoutes = require("./routes/likesRoutes.js");
+app.use("/api/likes", likesRoutes);
+
+let commentsRoutes = require("./routes/commentsRoutes.js");
+app.use("/api/comments", commentsRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to my Blogging API application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
