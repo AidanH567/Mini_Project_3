6555998cb7fd9577fbae1949d5fb487b8b483221
AidanH567 <herstikaidan@gmail.com>
2024-12-10 19:17:13 +1100

    empty branch

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  userController: require("./userController"),
-  postController: require("./postController"),
-  commentController: require("./commentController"), // Import the comment controller
-  likeController: require("./likeController"), // Import the like controller
-};
