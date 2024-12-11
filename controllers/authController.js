557a9ff7c9963dc9d6bf5f40371eaf2afe3bb7da
AidanH567 <herstikaidan@gmail.com>
2024-12-11 17:03:28 +1100

    deleted vite

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
-import dbConnect from "../dbConnect";
+import dbConnect from "../dbConnect.js";
+import bcrypt from "bcryptjs";
 export const register = (req, res) => {};
 
 //CHECK EXISTING USER
@@ -7,6 +8,13 @@ const q = "SELECT * FROM users WHERE emailId = ? OR username = ?";
 dbConnect.query(q, [req.body.emailId, req.body.lastName], (err, data) => {
   if (err) res.json(err);
   if (data.length) return res.status(409).json("User already exists");
+
+  //Hash Password and create a user
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync("req.body.password", salt);
+
+  const q = "INSERT INTO users(`username`, `email`,`password`) VALUES (?)";
+  const values = [req.body.username, req.body.username, hash];
 });
 
 export const login = (req, res) => {};
